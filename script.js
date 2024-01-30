f70805996e83d5603eced1c29270c00c89ff2c14
Sabella-8 <gsabella549@gmail.com>
2024-01-30 15:04:12 +0000

    sabella level 100

--- script.js
+++ script.js
@@ -5,8 +5,18 @@ function setup() {
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  for (episode of episodeList) {
+    const movieList = document.getElementById("root").content.cloneNode(true);
+    movieList.querySelector("h1").textContent = episode.name;
+    const spu = document.createElement("span");
+    spu.textContent = ` - S${String(episode.season).padStart(2, "0")}`;
+    movieList.querySelector("h1").append(spu);
+    const epu = document.createElement("span");
+    epu.textContent = `E${String(episode.number).padStart(2, "0")}`;
+    movieList.querySelector("h1").append(epu);
+    movieList.querySelector("img").setAttribute("src", episode.image.medium);
+    movieList.querySelector("p").textContent = episode.summary;
+    document.body.append(movieList);
+  }
 }
-
 window.onload = setup;
