2a0eb754a64d2ac53f2bc3a274d1836fadade970
Sabella-8 <gsabella549@gmail.com>
2024-02-07 22:52:12 +0000

    level 200 (mine)

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-//You can edit ALL of the code here
 function setup() {
-  const allEpisodes = getAllEpisodes();
+  const allEpisodes = state.getAllEpisodes;
   makePageForEpisodes(allEpisodes);
+  const numb = document.getElementById("num-epis");
+  numb.textContent = " " + allEpisodes.length + " ";
 }
 
 function makePageForEpisodes(episodeList) {
+  const root = document.createElement("div");
+  root.innerHTML = "";
   for (episode of episodeList) {
     const movieList = document.getElementById("root").content.cloneNode(true);
     movieList.querySelector("h1").textContent = episode.name;
@@ -16,7 +19,28 @@ function makePageForEpisodes(episodeList) {
     movieList.querySelector("h1").append(epu);
     movieList.querySelector("img").setAttribute("src", episode.image.medium);
     movieList.querySelector("p").textContent = episode.summary;
-    document.body.append(movieList);
+    root.appendChild(movieList);
   }
+  document.body.appendChild(root);
 }
+
+function render() {
+  const searching = document.getElementById("search-bar");
+  const allEpisodes = state.getAllEpisodes;
+  searching.addEventListener("input", function () {
+    state.searchTerm = searching.value.toLowerCase();
+    const filteredEpisodes = allEpisodes.filter((episode) => {
+      return (
+        episode.name.toLowerCase().includes(state.searchTerm) ||
+        episode.summary.toLowerCase().includes(state.searchTerm)
+      );
+    });
+    const numb = document.getElementById("num-epis");
+    numb.textContent =
+      " " + filteredEpisodes.length + "/" + allEpisodes.length + " ";
+    makePageForEpisodes(filteredEpisodes);
+  });
+}
+
+render();
 window.onload = setup;
